16a1bb42dd9d3ff05fedc7141f3268e998714f01
ShalevAsor <shalevasor@gmail.com>
2025-07-27 12:15:44 +0300

    fix types import

--- src/types/index.ts
+++ src/types/index.ts
@@ -74,26 +74,35 @@ export type NextApiResponseServerIo = NextApiResponse & {
 export type MessageWithMember = (Message | DirectMessage) & {
   member: Member;
 };
-// export interface MessageWithMemberWithUser extends Message {
-//   member: Member & {
-//     user: User;
-//   };
-// }
-// export type MessagesResponse = {
-//   items: MessageWithMemberWithUser[];
-//   nextCursor?: string;
-// };
-// export type MessageWithMemberWithUser = Message & {
-//   member: Member & {
-//     user: User;
-//   };
-// };
 
-// export type DirectMessageWithMemberWithUser = DirectMessage & {
-//   member: Member & {
-//     user: User;
-//   };
-// };
+export type MessageWithMemberWithUser = Message & {
+  member: Member & {
+    user: User;
+  };
+};
+
+export type DirectMessageWithMemberWithUser = DirectMessage & {
+  member: Member & {
+    user: User;
+  };
+};
+
+export type PaginatedMessages = {
+  items: MessageWithMemberWithUser[];
+  nextCursor: string | undefined;
+};
+export type DirectMessageWithMemberDetails = DirectMessage & {
+  member: Member & {
+    user: User;
+    server: Server;
+  };
+};
+
+// Define type for paginated response
+export type PaginatedDirectMessages = {
+  items: DirectMessageWithMemberDetails[];
+  nextCursor: string | undefined;
+};
 
 export type ChatMessage =
   | MessageWithMemberWithUser
